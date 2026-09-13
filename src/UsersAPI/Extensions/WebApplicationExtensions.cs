5dd72f33e23eb9e6f54f66d17a7b2eeb08201409
Maicon Guedes <maicon.ag17@hotmail.com>
2026-09-12 21:20:37 -0300

    Adiciona suporte a métricas de desempenho com Prometheus na API

--- src/UsersAPI/Extensions/WebApplicationExtensions.cs
+++ src/UsersAPI/Extensions/WebApplicationExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using UsersAPI.Api.Middlewares;
 using UsersAPI.Api.Options;
+using Prometheus;
 
 namespace UsersAPI.Api.Extensions
 {
@@ -7,6 +8,9 @@ namespace UsersAPI.Api.Extensions
     {
         public static WebApplication UseApiPresentation(this WebApplication app)
         {
+            app.UseRouting();
+            app.UseWhen(context => context.Request.Path.StartsWithSegments("/api"),
+                pipeline => pipeline.UseHttpMetrics());
             UseRequestLogging(app);
             UseExceptionHandling(app);
             UseApiDocumentation(app);
@@ -50,6 +54,7 @@ namespace UsersAPI.Api.Extensions
             app.UseAuthentication();
             app.UseAuthorization();
             app.MapControllers();
+            app.MapMetrics("/metrics").AllowAnonymous();
         }
     }
 }
